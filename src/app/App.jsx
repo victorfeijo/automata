fe0e2994a634cfcd6a88f958a72b9a933a116e7c
Victor Feijo <victor.feijoa@gmail.com>
2017-03-08 18:25:44 -0300

    refactor to be more beauty stateless component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { DatePicker } from 'antd';
 import './App.css';
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <DatePicker />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Hello World</h1>
+    <DatePicker />
+  </div>
+);
+
+export default App;
