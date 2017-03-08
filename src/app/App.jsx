43ef3c1996ca4bcabb63c5af1aef8ecc2b4a9466
Victor Feijo <victor.feijoa@gmail.com>
2017-03-08 17:26:00 -0300

    now with react :cow:

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('some 2')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
