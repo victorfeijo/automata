4e10dd72743807b6e5cf65516700443b5da6e815
Victor Feijo <victor.feijoa@gmail.com>
2017-03-08 20:44:58 -0300

    improve server file

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const path = require('path');
 
-const app = express()
-const publicDir = __dirname + '/dist'
-const port = process.env.PORT || 3000
+const app = express();
+const publicDir = __dirname + '/dist';
+const port = process.env.PORT || 3000;
 
 function errorHandler(err, req, res, next) {
   if (res.headersSent) {
-      return next(err)
-    }
-  res.status(500)
-  res.render('error', { error: err })
+    return next(err);
+  }
+  res.status(500);
+  res.render('error', { error: err });
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(publicDir, '/index.html'))
-})
+  res.sendFile(path.join(publicDir, '/index.html'));
+});
 
-app.use(errorHandler)
-app.use(express.static(__dirname + '/dist'))
+app.use(errorHandler);
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(port, () => {
-  console.log('Starting server on port  ' + port)
-})
+  console.log('Starting server on port  ' + port);
+});
