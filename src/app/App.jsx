752475476cb5ce19c5d887b6751fc2428dc940f4
Victor Feijo <victor.feijoa@gmail.com>
2017-03-08 18:06:01 -0300

    adds linter command

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { DatePicker } from 'antd';
 import './App.css';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <h1>Hello World</h1>
-        <DatePicker />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <DatePicker />
+    </div>
+  );
 }
