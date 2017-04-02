51d42aa059cbb0feb1d013a9d1ee3eb40fbe99ad
Victor Feijo <victor.feijoa@gmail.com>
2017-04-02 20:40:51 -0300

    bye bye css, welcome styled components

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { DatePicker } from 'antd';
-import './App.css';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  text-align: center;
+  font-size: 20px;
+`;
+
+const Title = styled.h1`
+  font-weight: 600;
+`;
 
 const App = () => (
-  <div className="App">
-    <h1>Hello World</h1>
+  <Content>
+    <Title>Hello World</Title>
     <DatePicker />
-  </div>
+  </Content>
 );
 
 export default App;
